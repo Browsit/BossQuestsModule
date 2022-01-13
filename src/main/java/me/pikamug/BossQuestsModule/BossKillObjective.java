ab93894bc3f0c05bea6764b462cda0e4c54ad199
PikaMug <2267126+PikaMug@users.noreply.github.com>
2022-01-13 12:27:25 -0500

    Compatibility with Boss 4.0.0+. Bump version

--- src/main/java/me/pikamug/BossQuestsModule/BossKillObjective.java
+++ src/main/java/me/pikamug/BossQuestsModule/BossKillObjective.java
@@ -1,29 +1,47 @@
+/*
+ * Copyright (c) 2019 PikaMug and contributors. All rights reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+ * NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package me.pikamug.BossQuestsModule;
 
-import java.util.Map;
-
-import org.bukkit.Bukkit;
+import me.blackvein.quests.CustomObjective;
+import me.blackvein.quests.Quest;
+import me.blackvein.quests.Quester;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.mineacademy.boss.api.event.BossDeathEvent;
 
-import me.blackvein.quests.CustomObjective;
-import me.blackvein.quests.Quest;
-import me.blackvein.quests.Quester;
-import me.blackvein.quests.Quests;
+import java.util.Map;
 
 public class BossKillObjective extends CustomObjective implements Listener {
-	private static Quests quests = (Quests) Bukkit.getServer().getPluginManager().getPlugin("Quests");
 	
 	public BossKillObjective() {
 		setName("Boss Kill Mobs Objective");
 		setAuthor("PikaMug");
 		setShowCount(true);
-		addStringPrompt("Kill Obj", "Set a name for the objective", "Kill Boss");
-		addStringPrompt("Kill Names", "Enter Boss names, separating each one by a comma", "ANY");
+		addStringPrompt("Boss Kill Obj", "Set a name for the objective", "Kill Boss");
+		addStringPrompt("Boss Kill Names", "Enter Boss names, separating each one by a comma", "ANY");
 		setCountPrompt("Set the amount of Boss to kill");
-		setDisplay("%Kill Obj% %Kill Names%: %count%");
+		setDisplay("%Boss Kill Obj% %Boss Kill Names%: %count%");
+	}
+
+	@Override
+	public String getModuleName() {
+		return BossModule.getModuleName();
+	}
+
+	@Override
+	public Map.Entry<String, Short> getModuleItem() {
+		return BossModule.getModuleItem();
 	}
 	
 	@EventHandler
@@ -32,16 +50,16 @@ public class BossKillObjective extends CustomObjective implements Listener {
 		if (killer == null) {
 			return;
 		}
-		Quester quester = quests.getQuester(killer.getUniqueId());
+		Quester quester = BossModule.getQuests().getQuester(killer.getUniqueId());
 		if (quester == null) {
 			return;
 		}
 		String mobName = event.getBoss().getName();
-		String customMobName = event.getBoss().getSettings().getCustomName();
+		String customMobName = event.getBoss().getAlias();
 		for (Quest q : quester.getCurrentQuests().keySet()) {
 			Map<String, Object> datamap = getDataForPlayer(killer, this, q);
 			if (datamap != null) {
-				String mobNames = (String)datamap.getOrDefault("Kill Names", "ANY");
+				String mobNames = (String)datamap.getOrDefault("Boss Kill Names", "ANY");
 				if (mobNames == null) {
 					return;
 				}
